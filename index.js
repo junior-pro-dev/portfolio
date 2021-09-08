7f881736eee207e0bb8a50b31bca52922bad0b34
junior-pro-dev <90272949+junior-pro-dev@users.noreply.github.com>
2021-09-08 16:40:12 -0500

    Removed extra space

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const navButton = document.getElementById('nav-button');
 
-
 const handleClick = () => alert('you must have allowed JavaScript in the browser for this website');
 
 navButton.addEventListener(

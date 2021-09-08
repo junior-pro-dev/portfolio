add8057f293aaaea3d26e1507fb389b41bd21ed5
junior-pro-dev <90272949+junior-pro-dev@users.noreply.github.com>
2021-09-08 16:01:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const navButton = document.getElementById('nav-button');
 
+
+const handleClick = () => alert('you must have allowed JavaScript in the browser for this website');
+
 navButton.addEventListener(
   'click',
-  () => alert('you must have allowed JavaScript in the browser for this website');
+  handleClick
 );

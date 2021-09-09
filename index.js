5cd1ecd413d4e0a53a965fad5ac907de2ee610f3
junior-pro-dev <90272949+junior-pro-dev@users.noreply.github.com>
2021-09-08 20:48:42 -0500

    Temporal change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const navButton = document.getElementById('nav-button');
 
 const toggleVisibility = () => {
   const navPopup = document.getElementById('nav-popup');
-  navPopup.toggleAttribute('hidden');
+  navPopup.classList.add('hidden');
   alert(1);
 }
 // const t = () => alert('you must have allowed JavaScript in the browser for this website');

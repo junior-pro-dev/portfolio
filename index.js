61e18b60f547eb8cb28a0e93db633e48472cfdbb
junior-pro-dev <90272949+junior-pro-dev@users.noreply.github.com>
2021-09-09 13:21:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const navButton = document.getElementById('nav-button');
 // let navPopup;
 
 const toggleVisibility = () => {
-  const navPopup = document.getElementById('nav-popup');
+  const navPopup = document.getElementById('nav-popup-id');
   navPopup.classList.toggle('nav-hidden');
+  navPopup.classList.toggle('nav-popup');
 }
 // const t = () => alert('you must have allowed JavaScript in the browser for this website');
 
